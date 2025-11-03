69fde3f60d2526a2babfe99d45f4979da8f961d7
lucasalves17-jpg <lucas.alves17@escola.pr.gov.br>
2025-11-03 10:05:24 -0300

    a

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ console.log(avanca)
 
 avance.forEach(button ={
     buttton.addEventlistener(`click`,function(){
-          conts atual = document.querySelector(`ativo`)
-const proximopasso= "passo-" +this.getattribute(`.ativo`)
-atual.classlist. remove (`ativo`)
-document.getElementbyld(proximopasso).classlist.add(`ativo`)
+          conts atual = document.querySelector('.ativo')
+const proximopasso= "passo-" +this.getattribute('ativo')
+atual.classlist. remove ('ativo')
+document.getElementbyld(proximopasso).classlist.add('ativo')
 })
 }
 
